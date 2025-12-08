c1bbe7d3bac62d1f68d5e8fb16da09697e5e4871
杨彭伟 <pengwei.yang@highlands.ltd>
2025-12-08 16:03:03 +0800

    feat(bar_chart): 为背景柱状图添加padding和borderRadius支持
    允许自定义背景柱状图的内边距和圆角半径，提供更灵活的样式控制

--- lib/src/chart/bar_chart/bar_chart_data.dart
+++ lib/src/chart/bar_chart/bar_chart_data.dart
@@ -574,6 +574,8 @@ class BackgroundBarChartRodData with EquatableMixin {
     bool? show,
     Color? color,
     this.gradient,
+    this.padding = EdgeInsets.zero,
+    this.borderRadius,
   })  : fromY = fromY ?? 0,
         toY = toY ?? 0,
         show = show ?? false,
@@ -582,6 +584,10 @@ class BackgroundBarChartRodData with EquatableMixin {
 
   /// Determines to show or hide this
   final bool show;
+
+  final EdgeInsets padding;
+
+  final BorderRadius? borderRadius;
 
   /// [fromY] is where background starts to show
   final double fromY;
@@ -611,6 +617,7 @@ class BackgroundBarChartRodData with EquatableMixin {
         color: Color.lerp(a.color, b.color, t),
         gradient: Gradient.lerp(a.gradient, b.gradient, t),
         show: b.show,
+        padding: EdgeInsets.lerp(a.padding, b.padding, t)!,
       );
 
   /// Used for equality check, see [EquatableMixin].
@@ -621,6 +628,7 @@ class BackgroundBarChartRodData with EquatableMixin {
         toY,
         color,
         gradient,
+        padding,
       ];
 }
 
